56a74ed22b022b5e06d440f618004b3d39d72951
fruzelee <fazle.rabbi.cse@gmail.com>
2023-07-09 17:55:53 +0600

    feat: get the bytes array from the uri

---
+++ app/src/main/java/com/github/fruzelee/androidworkmanager/PhotoCompressionWorker.kt
@@ -0,0 +1,29 @@
+package com.github.fruzelee.androidworkmanager
+
+import android.content.Context
+import android.net.Uri
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+
+class PhotoCompressionWorker(
+    private val appContext: Context,
+    private val params: WorkerParameters
+) : CoroutineWorker(appContext, params) {
+    override suspend fun doWork(): Result {
+        val stringUri = params.inputData.getString(KEY_CONTENT_URI)
+        val compressionThresholdInBytes = params.inputData.getLong(
+            KEY_COMPRESSION_THRESHOLD,
+            0L
+        )
+        val uri = Uri.parse(stringUri)
+        val bytes = appContext.contentResolver.openInputStream(uri)?.use {
+            it.readBytes()
+        } ?: return Result.failure()
+    }
+
+    companion object {
+        const val KEY_CONTENT_URI = "KEY_CONTENT_URI"
+        const val KEY_COMPRESSION_THRESHOLD = "KEY_COMPRESSION_THRESHOLD"
+        const val KEY_RESULT_PATH = "KEY_RESULT_PATH"
+    }
+}
