03c8b92c18180f9a458e2314542fb5e6016dbd2a
fruzelee <fazle.rabbi.cse@gmail.com>
2023-07-11 11:44:04 +0600

    feat: Display images using Compose code
    This commit introduces a new feature to display images using Compose code. The changes enable rendering and visual presentation of images within the application.

--- app/src/main/java/com/github/fruzelee/androidworkmanager/MainActivity.kt
+++ app/src/main/java/com/github/fruzelee/androidworkmanager/MainActivity.kt
@@ -8,16 +8,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.work.Constraints
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.workDataOf
+import coil.compose.AsyncImage
 import com.github.fruzelee.androidworkmanager.ui.theme.AndroidWorkManagerExampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -53,9 +63,26 @@ class MainActivity : ComponentActivity() {
                             viewModel.updateCompressBitmap(bitmap)
                         }
                     }
-
                 }
 
+                // display the images
+
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+
+                ) {
+                    viewModel.uncompressedUri?.let {
+                        Text(text = "Uncompressed photo:")
+                        AsyncImage(model = it, contentDescription = null)
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    viewModel.compressedBitmap?.let {
+                        Text(text = "Compressed photo:")
+                        Image(bitmap = it.asImageBitmap(), contentDescription = null)
+                    }
+                }
             }
         }
     }
